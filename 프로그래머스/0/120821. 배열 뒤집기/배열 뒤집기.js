eebaf47dcbce91f78f13bba9b6b2a8080fb9f492
Hongjong!n <91616495+hjiasdq@users.noreply.github.com>
2025-08-26 10:49:43 +0900

    Update 배열 뒤집기.js

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -2,3 +2,9 @@ function solution(num_list) {
     let arrayRevers = num_list.reverse()
     return arrayRevers
 }
+
+//원본을 유지한채로 뒤집고 싶다면
+
+function solution(num_list) {
+    return [...num_list].reverse(); 
+}
